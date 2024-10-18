d91f688a8ebed84144e44c56c309496a609d0565
Stel Clementine <stel@stel.codes>
2024-10-17 21:19:58 -0700

    Add CSS reset

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	import '../reset.css';
 </script>
 
-<h1>Hello world</h1>
+<div class="app-container">
+	<h1>Hello world</h1>
+</div>
 
 <style lang="scss">
+	.app-container {
+    height: 100vh;
+	}
 </style>
