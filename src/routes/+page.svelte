fac5ee832e3fbbded8ff7e08dc00095d10b5a0d9
Stel Clementine <stel@stel.codes>
2024-10-18 00:44:23 -0700

    Create mvp website with title and date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,23 @@
 
 <div class="app-container">
 	<header>
+		<img class="tcwp" alt="the collective womb presents" src="./images/tcwp.gif" />
+    <img class="divider" alt="barbed wire" src="./images/barbed-wire-divider.png" />
+		<img class="hell" alt="hell" src="./images/hell.gif" />
+		<div class="abh">@ bitch house</div>
 	</header>
-	<main></main>
+	<main>
+		<h1>hell at bitch house</h1>
+		<div class="date">
+			<p>Saturday, October 26th</p>
+			<p>8 PM - ???</p>
+		</div>
+	</main>
 	<footer>
-		<p>Made by Stel Clementine 🎀</p>
+		<div class="text">
+			<p>made by stel clementine</p>
+			<span>🎀</span>
+		</div>
 	</footer>
 </div>
 
@@ -17,27 +30,95 @@
 	@function rem($pxValue) {
 		@return math.div($pxValue, 16px) * 1rem;
 	}
+	@font-face {
+		font-family: 'crystopia';
+		src: url('./fonts/crystopia.ttf');
+		font-weight: 400;
+		font-style: normal;
+		font-display: swap;
+	}
+	@font-face {
+		font-family: 'mr-monstar';
+		src: url('./fonts/mr-monstar.ttf');
+		font-weight: 400;
+		font-style: normal;
+		font-display: swap;
+	}
 	.app-container {
 		display: flex;
 		flex-direction: column;
-		height: 100vh;
-	}
-	header {
-		flex: 0 1 auto;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
-	main {
-		flex: 1 1 auto;
-	}
-	footer {
-		flex: 0 0 auto;
-		display: flex;
-		justify-content: center;
-		padding: rem(6px);
-		background: rgba(0, 0, 0, 0.5);
+		background: rgba(0, 0, 0, 0.3) url('./images/ghosts.png');
+		background-blend-mode: darken;
+		min-height: 100vh;
 		color: white;
-	}
+		cursor: url('https://cur.cursors-4u.net/holidays/hol-4/hol333.png'), pointer;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+
+    header, main, footer {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+    }
+
+		header {
+			flex: 0 1 auto;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+
+			img {
+				max-width: 100%;
+			}
+
+			.tcwp {
+				width: 800px;
+				padding: rem(10px);
+			}
+
+			.abh {
+				font-family: cursive;
+				font-size: rem(22px);
+				padding: rem(16px);
+			}
+		}
+
+		main {
+			flex: 1 1 auto;
+
+			h1 {
+				visibility: hidden;
+			}
+
+      .date {
+        width: 700px;
+        max-width: 100%;
+        background: rgba(0, 0, 0, 0.5);
+        margin: 0 rem(32px) rem(32px);
+        padding: rem(16px);
+
+        p {
+          font-size: rem(30px);
+          text-align: center;
+          padding: rem(4px) 0;
+        }
+      }
+		}
+		footer {
+			flex: 0 0 auto;
+			padding: rem(6px);
+			background: rgba(0, 0, 0, 0.5);
+			color: white;
+			font-family: crystopia;
+			font-size: rem(24px);
+
+			.text {
+				display: flex;
+				align-items: center;
+
+				p {
+					padding: 0 rem(8px) rem(8px) 0;
+				}
+			}
+		}
 	}
 </style>
