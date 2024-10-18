ede2d0761d8103a16880ae9becca31488b9a3694
Stel Clementine <stel@stel.codes>
2024-10-17 22:02:13 -0700

    Add basic app container styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,41 @@
 </script>
 
 <div class="app-container">
-	<h1>Hello world</h1>
+	<header>
+	</header>
+	<main></main>
+	<footer>
+		<p>Made by Stel Clementine 🎀</p>
+	</footer>
 </div>
 
 <style lang="scss">
+	@use 'sass:color';
+	@use 'sass:math';
+	@function rem($pxValue) {
+		@return math.div($pxValue, 16px) * 1rem;
+	}
 	.app-container {
-    height: 100vh;
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+	}
+	header {
+		flex: 0 1 auto;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	main {
+		flex: 1 1 auto;
+	}
+	footer {
+		flex: 0 0 auto;
+		display: flex;
+		justify-content: center;
+		padding: rem(6px);
+		background: rgba(0, 0, 0, 0.5);
+		color: white;
+	}
 	}
 </style>
